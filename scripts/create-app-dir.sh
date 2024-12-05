21c4a9a4f0bdc0db024040abd76eec83c96ebbc6
josh <josh@mercury.local>
2024-12-05 14:14:25 -0800

    update script logic

--- scripts/create-app-dir.sh
+++ scripts/create-app-dir.sh
@@ -9,8 +9,8 @@
 # shellcheck enable=require-variable-braces
 
 
-SERVICE_NAME="${1?error}"; declare -r SERVICE_NAME
-SUBDIR_NAMES="${2?error}"; declare -r SUBDIR_NAMES
+NEW_SERVICE="${1?error}"; declare -r NEW_SERVICE
+NEW_SERVICE_DIRS="${2?error}"; declare -r NEW_SERVICE_DIRS
 
 # ENV SETTINGS ##########
 set -e                      # exit all shells if script fails
@@ -22,54 +22,35 @@ IFS=$'\n\t'
 
 
 
-declare -r SERVICES_DIRPATH='/volume2/docker-data/services'
+declare -r ROOT_DIRPATH='/volume2/docker-data/services'
 
 function run {
   local -r cmd="${1?error}"
-  ssh -t nas "${cmd?error}"':; exit ${?};' || exit 1
+  ssh -t nas "${cmd?error}"':; exit "${?}";' || exit 1
 }
 
 
 function main {
-  local -r service_dirpath="${SERVICES_DIRPATH?error}/${SERVICE_NAME?error}"
-  local -r app_data_dirpath="/app-data"
-  IFS=',' read -r -a subdir_names <<< "${SUBDIR_NAMES?error}"
+  local -r new_service_dirpath="${ROOT_DIRPATH?error}/${NEW_SERVICE?error}"
+  local -r new_service_app_data="${new_service_dirpath?error}/app-data"
 
+  IFS=',' read -ra new_service_dirs <<< "${NEW_SERVICE_DIRS?error}"
 
   local new_path=''
   local cmd=''
-  for subdir_name in "${subdir_names[@]}"; do
-    new_path="/${subdir_name?error}"
-    cmd="${cmd?error}"':; sudo mkdir -p '"'${new_path?error}';"
+
+  for dir in "${new_service_dirs[@]}"; do
+    new_path="${new_service_app_data?error}/${dir?error}"
+    cmd="${cmd?error}"'sudo mkdir -p '"'${new_path?error}';"
   done
 
 
-  cmd="${cmd?error}"' :; sudo chown --recursive 911:1001 '"''"
-  cmd="${cmd?error}"'; sudo chmod --recursive 755 '"'';"
+  cmd="${cmd?error}"'sudo chown --recursive 911:1001 '"'${new_service_dirpath}'"';'
+  cmd="${cmd?error}"'sudo chmod --recursive 755 '"'${new_service_dirpath}'"';'
 
+  # cmd="sudo sh -c '${cmd}'"
 
-  echo "EXECUTING: "
+  echo "EXECUTING: ${cmd}"
   run "${cmd?error}"
-
-
-  # echo "CREATING: "
-
-#   send 'mkdir -p'
-#   ssh nas <<'ENDSSH'
-#     readonly SERVICE_DIR='/var/services/homes/freon/docker-data/services'
-#     cd "" || exit 1
-#
-#     mkdir -p "/${SERVICE_NAME"}
-#     if [[ "$(pwd)" == "{SERVICE_DIR}" ]]; then
-#
-#
-#     else
-#       echo "ERROR | pwd: $(pwd) but expected "
-#       exit 1
-#     fi
-#
-#   ENDSSH
-
-
 }
 main
